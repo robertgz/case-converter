2038c2d1a745cb5a7aaa7fdd247910b2a19aaa37
Robert <robertgz@users.noreply.github.com>
2025-12-01 18:24:31 -0800

    update page footer

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
 
         <footer className="footer mt-auto py-3">
           <div className="container">
-            <span className="footer-text">&copy; 2019 Robert Garza | Created using React and reactstrap.</span>
+            <span className="footer-text">&copy; 2019 | Created using React and reactstrap.</span>
           </div>
         </footer>
                   
